84064631d4b12b898190c41e6b50358dd3a44a41
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-04-26 18:14:32 +0000

    adding documentation

--- free_array.c
+++ free_array.c
@@ -1,5 +1,11 @@
 #include "shell.h"
 
+/**
+ * free_array - free a 2D array of strings and its contents
+ *
+ * @token: the 2D array of strings to free
+ */
+
 void free_array(char **token)
 {
 	int i = 0;
