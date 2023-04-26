58005674667d88161df8a7fee7535c8634998a58
JonathanPR24 <jonathanperez604@gmail.com>
2023-04-26 16:08:39 +0000

    pass betty

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include "shell.h"
 
-int main(__attribute__((unused)) int ac, __attribute__((unused))char **argv)
+/**
+ * main - Entry Point
+ * @ac: variable not used
+ * @argv: variable not used
+ * Return: 0 on SUCCESS
+ */
+
+int main(__attribute__((unused)) int ac, __attribute__((unused)) char **argv)
 {
 	char *prompt = "$ ";
 	char *lineptr = NULL;
@@ -12,33 +19,30 @@ int main(__attribute__((unused)) int ac, __attribute__((unused))char **argv)
 	while (!should_exit)
 	{
 		save = NULL;
-		lineptr = NULL;
 		n = 0;
-		if (isatty(0))
+		if (isatty(STDIN_FILENO))
 			printf("%s", prompt);
+
 		char_read = getline(&lineptr, &n, stdin);
+
 		if (char_read == -1)
-		{
-			free(lineptr);
 			exit(EXIT_SUCCESS);
-		}
+
 		save = parser(lineptr, " \n");
 		free(lineptr);
+
 		if (save[0])
 		{
 			if (strcmp(save[0], "exit") == 0)
-			{
 				exec_exit();
-				should_exit = 1;
-			}
+			else if (strcmp(save[0], "env") == 0)
+				print_env();
 			else
 				exec(save);
 		}
-		else
-			free(save);
+
+		free(save);
 	}
 
-	free(lineptr);
-	free_array(save);
 	return (0);
 }
